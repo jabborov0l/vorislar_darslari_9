fcf79b5d16d77b3929fd6fc905781c1733855cfc
jabborov0l <masharipovumid00@gmail.com>
2025-10-09 16:29:17 +0500

    ikkinchi dars github

---
+++ 2-dars.py
@@ -0,0 +1,25 @@
+# Bugundan PYTHON bilan ishlaymiz
+# Print() konsolga ma'lumot chiqarish buyrug'i
+print('salom barchangizga')
+print("Mayli salom dunyo")
+print("""Bu nima degani?""")
+
+# Arifmetik amallar
+# Qo'shish      
+a = 12
+b = 34
+c = a + b
+print('c=', c)
+
+# Ayirish 
+v = a - b
+print('v=', v)
+
+# Ko'paytirish
+m = a * b
+print('m=', m)
+
+# Bo'lish
+g = 5
+p1 = g / 2 # haqiqiy qismini olish
+p2 = g // 2 # butun qismini olish
