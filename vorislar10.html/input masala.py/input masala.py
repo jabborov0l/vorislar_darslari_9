78007ce693fcedf8724d5574a82feac6b0679011
jabborov0l <masharipovumid00@gmail.com>
2025-10-16 15:29:26 +0500

    masala

--- vorislar10.html/input masala.py/input masala.py
+++ vorislar10.html/input masala.py/input masala.py
@@ -1,3 +1,5 @@
+import math
+
 ## input aperatoridan foydalanib masala yechish
 # a = int(input(istalgan son kiriting menh uni kub ildizini aniqlab beraman.))
 # n = a * a * a
@@ -8,22 +10,43 @@
 
 
 ## kvadratning tomoni berilgan uning yuzasi aniqlansin. S = a * a
-a = int(input("kvadratning tomonini kiriting :",))
-S = a * a
-print("kvadratning yuzasi : S = ", S)
+# a = int(input("kvadratning tomonini kiriting :",))
+# S = a * a
+# print("kvadratning yuzasi : S = ", S)
 
 
 
-## To'g'ri to'rtburchakning tomonlari a va b berilgan. Uning yuzasi S = a * b va P = 2*(a+b) perimetri aniqlansin
-a = int(input("To'g'ri to'rtburchagning 1-tomonini kiriting yuzasini aniqlab beraman:"))
-b = int(input("to'g'ri to'rtburchagning 1-tomonini kiriting yuzasini aniqlab beraman: "))
-S = a * b
-print("to'g'ri to'rtburchakning yuzasi: S = ", S)
+# ## To'g'ri to'rtburchakning tomonlari a va b berilgan. Uning yuzasi S = a * b va P = 2*(a+b) perimetri aniqlansin
+# a = int(input("To'g'ri to'rtburchagning 1-tomonini kiriting yuzasini aniqlab beraman:"))
+# b = int(input("to'g'ri to'rtburchagning 1-tomonini kiriting yuzasini aniqlab beraman: "))
+# S = a * b
+# print("to'g'ri to'rtburchakning yuzasi: S = ", S)
 
 
-## Aylananing diametri d berilgan. Uning hajmini V = a * a * a va to'la sirti S = 6 * a * a aniqlansin
-d = int(input("aylananing diametrini kiriting yuzasini aniqlab beraman: "))
-p = 3.14
-L = d * p
-print("aylana diametri yuzi:L =", d * p)
+# ## Aylananing diametri d berilgan. Uning hajmini V = a * a * a va to'la sirti S = 6 * a * a aniqlansin
+# d = int(input("aylananing diametrini kiriting yuzasini aniqlab beraman: "))
+# p = 3.14
+# L = d * p
+# print("aylana diametri yuzi:L =", d * p)
+
+
+# ## Ikkita son berilgan a va b ularning o'rta arifmetigi aniqlansin (a + b)/2
+# a = int(input("1-sonni kiriting men uni o'rta arifmetigini aniqlab beraman"))
+# b = int(input("2-sonni kiriting men uni o'rta arifmetigini aniqlab beraman"))
+# (a + b)/2
+
+
+# ## Ikkita manfiy bo'lmagan son a va b berilgan. Ularning o'rta geometrigi aniqlansin  
+# a = int(input("1-sonni  manfiy bo'lmagan son kiriting a = "))
+# b = int(input("2-sonni manfiy bo'lmagan son kiriting b ="))
+
+# o_geom = math.sqrt(a * b)
+# print("o'rta geometrigi : o_geometrik qiymati = ", o_geom )
+
+## nolga teng bo'lmagan ikkita yig'indisini ko'paytmasini va har birinng moduli aniqlansin.
+a = int(input("0 ga teng bo'lmagan 1-sonni  kiriting ")) 
+b = int(input("0 ga teng bo'lmagan 2-sonni kiriting "))
+c = math.sqrt(a * a + b *b)
+print(("0 ga teng bo'lmagan ikki sonni ko'paytmasi : o_geometrik qiymati =",))
+
 
