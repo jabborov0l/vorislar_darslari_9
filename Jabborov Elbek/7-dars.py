ab05a357767a81dfee92263b86f76ff8ebd1a4c3
jabborov0l <masharipovumid00@gmail.com>
2025-10-21 16:25:36 +0500

    7-dars

---
+++ Jabborov Elbek/7-dars.py
@@ -0,0 +1,41 @@
+# """
+#   21.10.2025
+#   Python list operators
+# """
+
+# ism = "Bobur"
+# uy_manzili = "Toshkent shahar, CHilonzor tumani"
+
+# print(ism.capitalize())
+
+# # Pythonda list(ro'yhatlar) bilan ishlash
+
+# hayvonlar = ["kuchuk", "ho'kiz", "ot", "quyon"]
+# oila = ["ota", 30, "ona", 27, "aka", 7, "uka", 5]
+# mashinalar = ["spark", "matiz", "damas", "Nexia", "cobalt"]
+
+# hayvonlar = ["kuchuk", "ho'kiz", "ot", "quyon"]
+# index          0         1       2      3
+# print(hayvonlar[2])
+# qiymat = hayvonlar[3]
+# print(qiymat)
+# print(hayvonlar[2].capitalize)
+
+fanlar = ["Matematika", "Ona tili", "Algebra", "Geometriya", "Rus tili", "Ingliz tili", "Adabiyot", "Musiqa", "Tarbiya", "Jismoniy madaniyat"]
+
+print(fanlar)
+print(fanlar[4])  
+
+IT_oquvchilar = []
+
+IT_oquvchilar.append("Anvar")
+IT_oquvchilar.append("Behruz")
+IT_oquvchilar.append("Diyor")
+IT_oquvchilar.append("Elyor")
+IT_oquvchilar.append("Fahriyor")
+print(IT_oquvchilar)
+
+IT_oquvchilar.insert(2, "Jasur")
+IT_oquvchilar.insert(0, "Humoyun")
+print(IT_oquvchilar)
+
