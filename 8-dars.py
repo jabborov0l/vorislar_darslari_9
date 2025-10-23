112ab6ca5df55ffbab2bf35d5ecea4f1f5edc660
jabborov0l <masharipovumid00@gmail.com>
2025-10-23 16:24:31 +0500

    8-dars

---
+++ 8-dars.py
@@ -0,0 +1,34 @@
+ism = "Jack", "John", "Khonor"
+print(ism)
+xabar = "salom John, ahvollarimg qalay"
+print(xabar)
+sonlar = 3, -5, 10
+print(sonlar)
+t_shaxslar = [ "Islom Karimov", "Amir Temur", "Chingizxon"]
+z_shaxslar = ["Shavkat Mirziyoyev", "Elon Musk"]
+t_shaxslar.pop(2)
+z_shaxslar.pop(1)
+print(t_shaxslar)
+print(z_shaxslar)
+friends = []
+friends.append("Jack")
+friends.append("John")
+friends.append("Khonor")
+friends.append("Honor")
+print(friends)
+friends.remove("John")
+friends.append("Yohn")
+friends.insert(2, "Tom")
+friends.insert(0, "Ben")
+print(friends)
+
+mehmonlar = []
+friends.pop(3)
+mehmonlar.append("Tom")
+print(mehmonlar)
+
+
+
+
+
+
