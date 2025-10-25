2077d346223b92a1431fbe227da1bf7ee7bfbb46
jabborov0l <masharipovumid00@gmail.com>
2025-10-25 17:54:53 +0500

    ro'yxatlar bn iwlaw

--- python.py
+++ python.py
@@ -1,65 +1,90 @@
 #Bugundan PYTHON dasturlash tilini o'rganamiz
 #print() konsolga malumot chiqarish uchun qo'llaniladi
-print('Hello,world')
-print("Hello,world")
-print("""Hello,world""")
+# print('Hello,world')
+# print("Hello,world")
+# print("""Hello,world""")
 
-print('salom \n o\'quvchilar')
-print("xayr kumushay o'quvchi")
-print("""Hello,world""")
+# print('salom \n o\'quvchilar')
+# print("xayr kumushay o'quvchi")
+# print("""Hello,world""")
 
-#arifmetic Operators
-# +, -, #, /, //, %, **
-# + qo'shish
-
-
-a = 12
-b = 8
-c = a + b 
-print("c =", c)
+# #arifmetic Operators
+# # +, -, #, /, //, %, **
+# # + qo'shish
 
 
+# a = 12
+# b = 8
+# c = a + b 
+# print("c =", c)
 
-# - ayirish
-a = 10
-b = 2
-c = 8    
 
 
-#ko'paytirish
-e = a * b
-print("e = ", e)
+# # - ayirish
+# a = 10
+# b = 2
+# c = 8    
+
+
+# #ko'paytirish
+# e = a * b
+# print("e = ", e)
  
-# Bo'lish
-a = 32
-b = 8
-c = a // b
-print("c =", c)
+# # Bo'lish
+# a = 32
+# b = 8
+# c = a // b
+# print("c =", c)
 
-#input function
-ism = input("ismingiz nima?")
-print("salom hurmatli", + ism)
-
-
-#input function
-ism = input("ismingiz nima?")
-ism = input("otangizni ismi nima?")
-ism = input("onangizni ismi nima?")
-ism = input("ukangizni ismi nima?")
-print("salom hurmatli", + ism)
-ism = input(oyshajon)
+# #input function
+# ism = input("ismingiz nima?")
+# print("salom hurmatli", + ism)
 
 
+# #input function
+# ism = input("ismingiz nima?")
+# ism = input("otangizni ismi nima?")
+# ism = input("onangizni ismi nima?")
+# ism = input("ukangizni ismi nima?")
+# print("salom hurmatli", + ism)
+# ism = input("oyshajon")
 
 
+# """  10-sinf 
+#    25.10.2025.
+# Ro'yxatlar bn ishlash. Metodlar va functionlar bn ishlash
+# methods(.sort())
+# function(sum(), min(), max(), sorted(), list(), range())
+# Ro'yxatlardan nusxa olish
+# Tuple() o'zgarmas ro'yxat
+# ro'yxatlar icida ro'yxatlar"""
 
 
+# hayvonlar = []
+# gullar = list(())
+# print(hayvonlar)
+# print(gullar)
 
+# gullar.append("atrgul, lola")
+# gullar.append("lola, sariq")
+# gullar.append("atrgul, ko'kgul")
 
+# hayvonlar.insert(0,"it , muwuk")
+# hayvonlar.insert(1,"sicon, ewak")
+# hayvonlar.insert(2,"mol, ciyabo'ri")
 
+# print(hayvonlar)
+# print(gullar)
 
+# gullar.sort()
+# print(gullar)
 
+# hayvonlar.sort()
+# print(hayvonlar)
 
+# toq_sonlar = list(range(1, 100, 2))
+# print(toq_sonlar)
 
-
+# juft_sonlar = list(range(2, 101, 2))
+# print(juft_sonlar)
 
