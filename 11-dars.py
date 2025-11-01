0d41186b11a747994f5f3faf787ae1a9c2587e57
jabborov0l <masharipovumid00@gmail.com>
2025-11-01 16:16:18 +0500

    11-dars

---
+++ 11-dars.py
@@ -0,0 +1,31 @@
+oquvchilar = ["Jim", "Tom", "Jack", "Sam", "Klarck", "Jimmy"]
+# print(f"Assalomu alaykum{oquvchilar, 0}")
+# print(f"Assalomu alaykum{oquvchilar, 1}")
+# print(f"Assalomu alaykum{oquvchilar, 2}")
+# print(f"Assalomu alaykum{oquvchilar, 3}")
+# print(f"Assalomu alaykum{oquvchilar, 4}")
+# print(f"Assalomu alaykum{oquvchilar, 5}")
+
+# for ism in oquvchilar:
+#          print(f"Assalomu alaykum {ism}")
+#          print(f"Xayr hurmatli o'quvchi {ism}")
+
+# raqamlar = list((1,4,9,-8,63))
+# for son in raqamlar: 
+#          print(f"{son}sonlarni kvadrati:", {son ** 2})
+
+# sonlar = list(range(30, 91))
+# print(sonlar)
+# for son in sonlar:
+#     print(f"{son} sonlarni 9 ga bolganda javob:", {son/9})
+
+sonlar2 = []
+sonlar = list(range(27, 54, 7))
+
+print(f"Asl royhat : {sonlar}")
+for son in sonlar:
+    sonlar2.append(son**4)
+print(f"4-darajaga oshgan ro'yhat: {sonlar2}")
+
+
+
