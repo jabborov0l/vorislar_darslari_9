f6a5ea1d09c39327ae4218d46760612990945bd4
jabborov0l <masharipovumid00@gmail.com>
2025-11-04 16:33:16 +0500

    for sikliga oid masalala

---
+++ Jabborov Elbek/12-dars.py
@@ -0,0 +1,29 @@
+# For sikl operatoiga oid masalalar
+# For1.
+# k = int(input("k ni kiriting"))
+# n = int(input("n ni kiriting"))
+# for _ in range(n):
+#     print(k)
+
+# For2.
+# a = int(input("a ni kiriting"))
+# b = int(input("b ni kiriting"))
+# royhat = len(range(a,b+1))
+# for i in range(a, b+1):
+#        print(i)
+# print(royhat)
+
+# For3.
+# a = int(input("a ni kiriting"))
+# b = int(input("b ni kiriting"))
+# royhat = len(range(a+1,b))
+# royhat = sorted(range(a+1,b), reverse=True)
+# for i in range(b, a):
+#     print(i)
+# print(royhat)
+# print(len(royhat))
+#For4.
+# narx = int(input("1 kg konfet narxini kiriting"))
+# for i in range(1,11):
+#       print(i * narx)
+
