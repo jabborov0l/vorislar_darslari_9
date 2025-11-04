9199dc7f44215096d34fd0a27f724abab263d72d
jabborov0l <masharipovumid00@gmail.com>
2025-11-04 15:37:54 +0500

    my_portfolio

--- python.py
+++ python.py
@@ -88,3 +88,90 @@
 # juft_sonlar = list(range(2, 101, 2))
 # print(juft_sonlar)
 
+
+# ismlar = ["oyshajon", "kumushoy", "mohinur", "behruzbek"]
+# print(f"assalom alekum {ismlar[0].title()} qalaysizlar")
+
+# print(f"assalom alekum {ismlar[1].title()} qalaysizlar")
+
+# print(f"assalom alekum {ismlar[2].title()} qalaysizlar")
+
+# print(f"assalom alekum {ismlar[3].title()} qalaysizlar")
+
+# sonlar = list(range(1,41))
+# print(sonlar)
+
+# print(f"{sonlar[1]}ning kvadrati : {sonlar[1]**2}")
+# print(f"{sonlar[2]}ning kvadrati : {sonlar[2]**2}")
+# print(f"{sonlar[3]}ning kvadrati : {sonlar[3]**2}")
+# print(f"{sonlar[4]}ning kvadrati : {sonlar[4]**2}")
+# print(f"{sonlar[5]}ning kvadrati : {sonlar[5]**2}")
+# print(f"{sonlar[6]}ning kvadrati : {sonlar[6]**2}")
+# print(f"{sonlar[7]}ning kvadrati : {sonlar[7]**2}")
+# print(f"{sonlar[8]}ning kvadrati : {sonlar[8]**2}")
+# print(f"{sonlar[9]}ning kvadrati : {sonlar[9]**2}")
+# print(f"{sonlar[10]}ning kvadrati : {sonlar[10]**2}")
+# print(f"{sonlar[11]}ning kvadrati : {sonlar[11]**2}")
+# print(f"{sonlar[12]}ning kvadrati : {sonlar[12]**2}")
+# print(f"{sonlar[13]}ning kvadrati : {sonlar[13]**2}")
+# print(f"{sonlar[14]}ning kvadrati : {sonlar[14]**2}")
+# print(f"{sonlar[15]}ning kvadrati : {sonlar[15]**2}")
+# print(f"{sonlar[16]}ning kvadrati : {sonlar[16]**2}")
+# print(f"{sonlar[17]}ning kvadrati : {sonlar[17]**2}")
+# print(f"{sonlar[18]}ning kvadrati : {sonlar[18]**2}")
+# print(f"{sonlar[19]}ning kvadrati : {sonlar[19]**2}")
+# print(f"{sonlar[20]}ning kvadrati : {sonlar[20]**2}")
+# print(f"{sonlar[21]}ning kvadrati : {sonlar[21]**2}")
+# print(f"{sonlar[22]}ning kvadrati : {sonlar[22]**2}")
+
+# print(f"{sonlar[23]}ning kvadrati : {sonlar[23]**2}")
+# print(f"{sonlar[24]}ning kvadrati : {sonlar[24]**2}")
+# print(f"{sonlar[25]}ning kvadrati : {sonlar[25]**2}")
+# print(f"{sonlar[26]}ning kvadrati : {sonlar[26]**2}")
+# print(f"{sonlar[27]}ning kvadrati : {sonlar[27]**2}")
+# print(f"{sonlar[28]}ning kvadrati : {sonlar[28]**2}")
+# print(f"{sonlar[29]}ning kvadrati : {sonlar[29]**2}")
+# print(f"{sonlar[30]}ning kvadrati : {sonlar[30]**2}")
+# print(f"{sonlar[31]}ning kvadrati : {sonlar[31]**2}")
+# print(f"{sonlar[32]}ning kvadrati : {sonlar[32]**2}")
+# print(f"{sonlar[33]}ning kvadrati : {sonlar[33]**2}")
+# print(f"{sonlar[34]}ning kvadrati : {sonlar[34]**2}")
+# print(f"{sonlar[35]}ning kvadrati : {sonlar[35]**2}")
+# print(f"{sonlar[36]}ning kvadrati : {sonlar[36]**2}")
+# print(f"{sonlar[37]}ning kvadrati : {sonlar[37]**2}")
+# print(f"{sonlar[38]}ning kvadrati : {sonlar[38]**2}")
+# print(f"{sonlar[39]}ning kvadrati : {sonlar[39]**2}")
+# print(f"{sonlar[40]}ning kvadrati : {sonlar[40]**2}")
+
+
+# sonlar1 = list(range(-90,50,2))
+# print(sonlar1)
+# print(f"{sonlar1[3]}ning 9ga bo'linmasi {sonlar1[3]/9}")      
+
+# print(f"{sonlar1[mohinurca]}ni 9ga bo'lsak, {sonlar1[mohinurca]/9}")  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
