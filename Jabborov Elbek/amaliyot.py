d18a25fec26868ed165a25a2cd97784c11398302
jabborov0l <masharipovumid00@gmail.com>
2025-12-09 16:30:49 +0500

    Dictionary

---
+++ Jabborov Elbek/amaliyot.py
@@ -0,0 +1,4 @@
+# integer2.
+k = int(input("og'irligini kiriting"))
+tonna = k / 1000
+print("tonna:", tonna)
---
+++ dictionary.py
@@ -0,0 +1,19 @@
+thisdict = {
+    "Brand" : "Ford",
+    "Model" : "Mustang",
+    "Year"  : 1995
+}
+print(thisdict)
+
+Elbek = {
+    'ism' : "Elbek",
+    'familiya' : "Jabborov",
+    'yosh' : 15,
+    'manzil' : "Katta Jirmiz",
+    'maktab' : 46,
+    'sinf' : 9,
+    'Qiziqish' : "English",
+    'Transport' : "Tracker",
+    'Oqishi' : "Oliy"
+}
+print(f"Mening ismim {Elbek['ism']}. Mening familiyam {Elbek['familiya']}. Men {Elbek['yosh']} yoshdaman. Mening yashash manzilim {Elbek['manzil']}. Men {Elbek['maktab']}-maktabida o'qiyman. Mening sinfim {Elbek['sinf']}-sinfi. Mening qiziqishim {Elbek['Qiziqish']}tili. Bizming oilaviy transport {Elbek['Transport']}. Mening o'qish darajam {Elbek['Oqishi']}. ")
