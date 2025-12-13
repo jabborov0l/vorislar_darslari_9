6a0e55fc6317838c3fa566f86f96834b0d4e6686
jabborov0l <masharipovumid00@gmail.com>
2025-12-13 16:19:50 +0500

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -16,4 +16,16 @@ Elbek = {
     'Transport' : "Tracker",
     'Oqishi' : "Oliy"
 }
-print(f"Mening ismim {Elbek['ism']}. Mening familiyam {Elbek['familiya']}. Men {Elbek['yosh']} yoshdaman. Mening yashash manzilim {Elbek['manzil']}. Men {Elbek['maktab']}-maktabida o'qiyman. Mening sinfim {Elbek['sinf']}-sinfi. Mening qiziqishim {Elbek['Qiziqish']}tili. Bizming oilaviy transport {Elbek['Transport']}. Mening o'qish darajam {Elbek['Oqishi']}. ")
+# print(f"Mening ismim {Elbek['ism']}. Mening familiyam {Elbek['familiya']}. Men {Elbek['yosh']} yoshdaman. Mening yashash manzilim {Elbek['manzil']}. Men {Elbek['maktab']}-maktabida o'qiyman. Mening sinfim {Elbek['sinf']}-sinfi. Mening qiziqishim {Elbek['Qiziqish']}tili. Bizming oilaviy transport {Elbek['Transport']}. Mening o'qish darajam {Elbek['Oqishi']}. ")
+# ismlar = ["kumuwoy", "abdukarim", "oywajon"]
+# print(ismlar(1))
+# matn = print(talaba["ism"])   
+# print(talaba["yosh"])  
+
+Dictionary — bu ma’lumotlarni kalit (key) va qiymat (value) ko‘rinishida saqlaydigan tuzilma.
+dictionary1-Dictionary — bu ma’lumotlarni kalit (key) va qiymat (value) ko‘rinishida saqlaydigan tuzilma.
+Python’da dictionary (lug‘at) — bu kalit : qiymat ko‘rinishidagi ma’lumotlar to‘plami. U tartibsiz (order mumkin, lekin indeks bilan emas), o‘zgartiriladigan va kalitlar takrorlanmaydi.
+
+Quyida senga qisqa, tushunarli matn beraman.
+
+
